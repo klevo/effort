8d80f1940c24d444426f27c9aa9a2ecc75ee71fa
Robert Starsi <klevo@klevo>
2014-04-09 20:55:31 +0200

    fast commit

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,13 @@ module Effort
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Pretoria'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # http://www.iconoclastlabs.com/blog/bower-and-rails-4
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end
