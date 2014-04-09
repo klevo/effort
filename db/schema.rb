4a46ae964c66876eefc80ed03d5bb22606d55d0a
Robert Starsi <klevo@klevo>
2014-04-09 15:11:25 +0200

    added name to projects, ruby-version

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140409130032) do
+ActiveRecord::Schema.define(version: 20140409130934) do
 
   create_table "projects", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
