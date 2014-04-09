361280a97575252c7c0bf4fac9a2a6c735f9c5dd
Robert Starsi <klevo@klevo>
2014-04-09 15:54:44 +0200

    All projects header

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,8 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.last_updated
   end
+
+  def show
+    @project = Project.find params[:id]
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def create_page_title
+    if @project
+      @project.name
+    else
+      'Projects'
+    end
+  end
 end
