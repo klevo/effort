5dd489baa4e8689ea6413033bfedc446b9d3f920
Robert Starsi <klevo@klevo>
2014-04-09 17:22:38 +0200

    todo lists are added

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,11 +1,16 @@
 class TodoListsController < ApplicationController
   before_action :set_project
 
-  def new
-    @todo_list = @project.todo_lists.new
+  def create
+    @todo_list = @project.todo_lists.new(todo_list_params)
+    @todo_list.save!
   end
 
   private
+
+    def todo_list_params
+      params.require(:todo_list).permit(:name)
+    end
 
     def set_project
       @project = Project.find params[:project_id]
