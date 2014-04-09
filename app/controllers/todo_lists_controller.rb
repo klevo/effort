156870be3f5a3bf35c7e63813b72f670507f1702
Robert Starsi <klevo@klevo>
2014-04-09 17:08:04 +0200

    Adding new todo list

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,7 +1,13 @@
 class TodoListsController < ApplicationController
   before_action :set_project
 
-  def set_project
-    @project = Project.find params[:project_id]
+  def new
+    @todo_list = @project.todo_lists.new
   end
+
+  private
+
+    def set_project
+      @project = Project.find params[:project_id]
+    end
 end
