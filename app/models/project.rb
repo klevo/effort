e4838b32113ac21aab5db70846591e627515a86f
Robert Starsi <klevo@klevo>
2014-04-14 18:54:52 +0200

    fast commit

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,5 +12,7 @@ class Project < ActiveRecord::Base
     sorted_todo_list_ids.each_with_index do |todo_list_id, position|
       todo_lists.where(id: todo_list_id.to_i).update_all position: position
     end
+
+    touch
   end
 end
