65201c84c5cc2caa84884915105cca0c1d330860
Robert Starsi <klevo@klevo>
2014-04-16 12:22:00 +0200

    If all to-do items are marked as complete, to-do list is too

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -14,5 +14,16 @@ class TodoItem < ActiveRecord::Base
 
   def toggle
     update is_done: !is_done
+
+    # Update the status of the parent to-do list
+    if complete?
+      if !todo_list.completed? && todo_list.todo_items.pending.count == 0
+        todo_list.update completed: true
+      end
+    else
+      if todo_list.completed? && todo_list.todo_items.pending.count > 0
+        todo_list.update completed: false
+      end
+    end
   end
 end
