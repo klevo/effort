b805989029dd991027da7c282fd88bb7e8a4a1df
Robert Starsi <klevo@klevo>
2014-04-30 08:23:20 +0200

    test "toggle - one of all completed items, makes the list pending the list; touches project"

--- test/models/todo_item_test.rb
+++ test/models/todo_item_test.rb
@@ -4,7 +4,7 @@ class TodoItemTest < ActiveSupport::TestCase
   test "toggle - one pending item in a list with other completed, completes the list; touches project" do
     item = todo_items :vocab_database_three
     todo_list = todo_lists :vocab_database
-    refute todo_list.completed?
+    refute todo_list.complete?
     refute item.complete?
     
     now = Time.zone.local(2014, 4, 30)
@@ -23,6 +23,23 @@ class TodoItemTest < ActiveSupport::TestCase
   end
   
   test "toggle - one of all completed items, makes the list pending the list; touches project" do
+    item = todo_items :vocab_flasher_one
+    todo_list = todo_lists :vocab_flasher
+    assert todo_list.complete?
+    assert item.complete?
     
+    now = Time.zone.local(2014, 4, 30)
+    Timecop.travel now do
+      item.toggle
+    end
+    
+    item.reload
+    refute item.complete?
+    
+    todo_list.reload
+    refute todo_list.complete?
+    
+    project = projects :vocab
+    assert_equal now, project.updated_at
   end
 end
