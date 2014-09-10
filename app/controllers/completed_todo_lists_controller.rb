0bf241608f0a73467dc12675ba85a1c44b2847ab
Robert Starsi <klevoo@gmail.com>
2014-09-10 12:01:03 +0200

    completed todo lists are ordered by update date

--- app/controllers/completed_todo_lists_controller.rb
+++ app/controllers/completed_todo_lists_controller.rb
@@ -2,7 +2,7 @@ class CompletedTodoListsController < ApplicationController
   before_action :set_project
 
   def index
-    @todo_lists = @project.todo_lists.completed.positioned
+    @todo_lists = @project.todo_lists.completed.last_updated
     @pending_todo_lists_count = @project.todo_lists.pending.count
   end
 end
